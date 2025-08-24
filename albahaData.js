ea65b37579b20ef576a517734b7ceed7290ab1b5
mona2030liza-maker <mona.2030.liza@gmail.com>
2025-08-24 11:46:34 +0300

    Add albahaData.js 1756025195477

---
+++ albahaData.js
@@ -0,0 +1,50 @@
+// بيانات منطقة الباحة - المحافظات والمدن والقرى
+export const albahaRegionData = {
+  center: [20.0129, 41.4677], // إحداثيات مدينة الباحة
+  zoom: 10,
+  
+  governorates: [
+    {
+      id: 'albaha',
+      name: 'مدينة الباحة',
+      nameEn: 'Al Baha City',
+      population: 90515,
+      coordinates: [20.0129, 41.4677],
+      type: 'capital',
+      area: 'الحجاز'
+    },
+    {
+      id: 'baljurashi',
+      name: 'محافظة بلجرشي',
+      nameEn: 'Baljurashi',
+      population: 51787,
+      coordinates: [19.9167, 41.9167],
+      type: 'governorate',
+      area: 'السراة'
+    },
+    {
+      id: 'almandaq',
+      name: 'محافظة المندق',
+      nameEn: 'Al Mandaq',
+      population: 20010,
+      coordinates: [20.2333, 41.2833],
+      type: 'governorate',
+      area: 'تهامة'
+    },
+    {
+      id: 'almakhwah',
+      name: 'محافظة المخواة',
+      nameEn: 'Al Makhwah',
+      population: 48333,
+      coordinates: [19.7333, 41.3833],
+      type: 'governorate',
+      area: 'تهامة'
+    },
+    {
+      id: 'aqiq',
+      name: 'محافظة العقيق',
+      nameEn: 'Al Aqiq',
+      population: 37608,
+      coordinates: [20.3333, 41.6333],
+      type: 'governorate',
+      area: 'السراة'
